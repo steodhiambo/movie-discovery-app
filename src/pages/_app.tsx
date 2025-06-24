0c7dddba2db11f6849b32408d046c8676818f1ac
steodhiambo <stephenoginga6@gmail.com>
2025-06-24 17:37:31 +0300

    Integrate Navigation component into app layout
    - Add Navigation component to _app.tsx for global navigation
    - Remove redundant navigation elements from individual pages
    - Ensure consistent navigation across all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css'
 import { WatchlistProvider } from '@/context/WatchlistContext'
+import Navigation, { Footer } from '@/components/Navigation'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WatchlistProvider>
-      <Component {...pageProps} />
+      <div className="min-h-screen flex flex-col">
+        <Navigation />
+        <main className="flex-1">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </WatchlistProvider>
   )
 }
